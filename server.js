98b28da59c8dbe7d0b744f2f2e115209d0a1b966
Jeff Willden <jeff.willden@gmail.com>
2017-06-26 21:38:41 -0600

    Updates to port numbers

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 const ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const port = process.env.OPENSHIFT_NODEJS_PORT || 80;
 const environment = process.env.CHIASMUS_ENVIRONMENT || 'development';
 const Koa = require('koa');
 const app = new Koa();
@@ -25,9 +25,10 @@ app.use(async function (ctx, next) {
 // response
 
 app.use(ctx => {
-	ctx.body = 'Hello World';
+	ctx.body = 'Hello World on 80! IP: '+process.env.OPENSHIFT_NODEJS_IP+':'+process.env.OPENSHIFT_NODEJS_PORT;
 });
 
-app.listen(port, ipAddress, () => {
-	console.log('Listening on http://%s:%s', ipAddress, port);
+app.listen(port, () => {
+	console.log('Listening on port '+port);
+	console.log('OpenShift NodeJS IP: %s:%s',process.env.OPENSHIFT_NODEJS_IP,process.env.OPENSHIFT_NODEJS_PORT);
 });
